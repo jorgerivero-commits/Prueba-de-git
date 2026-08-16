0b90d6f4209d3ccf8c71bc998cc1a41601604e51
jorgerivero-commits <jorge.rivero@ucb.edu.bo>
2026-08-16 00:14:47 -0400

    Tercer commit: Preguntar

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Start small. Ship something.");
-        System.out.println("Hola mundo");
+        System.out.println("Como estan todos");
+
     }
 }
